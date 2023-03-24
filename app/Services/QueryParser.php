e884ee6007739ee7d6c381dbeef32ef31ed9478d
nico-dandrea <nicolaschisp@gmail.com>
2023-03-24 09:15:45 -0300

    Yes

--- app/Services/QueryParser.php
+++ app/Services/QueryParser.php
@@ -21,6 +21,8 @@ class QueryParser
         $this->queries = str($query)->explode(',');
     }
 
+
+
     /**
      * Tries to get the parts of the query has 3 keys, attribute, operator or value
      *
@@ -29,23 +31,34 @@ class QueryParser
      **/
     public function parts()
     {
-        $parts = $this->queries->map(function ($part) {
+        //TODO implement check for count == 1 and another for count >= 1
+
+        return $this->queries->map(function ($part) {
+
+            //Retrieves the operator part of the string
             $operator = collect([':', '=', '>', '<'])->first(fn ($op) => Str::contains($part, $op));
-            if (($attribute = Str::before($part, $operator)) == 'title' && Str::contains($operator, [':', '='])) {
+
+            //Retrieves the value part of the string
+            $value = trim(Str::after($part, $operator));
+
+            //Checks if the string contains a valid title operator
+            if (($attribute = trim(Str::before($part, $operator))) == 'title' && Str::contains($operator, [':', '='])) {
                 return collect(
                     [
                         'attribute' => $attribute,
                         'operator' => $operator,
-                        'value' => Str::after($part, $operator)
+                        'value' => $value
                     ]
                 );
             }
 
+            //Checks if the string contains a valid sale price operator
             if (!Str::contains($operator, ['>', '<']) && $attribute !== 'salePrice') {
                 throw new Exception("The query string is malformed", 400);
             }
 
-            if (!is_numeric($value = Str::after($part, $operator)) || $value <= 0) {
+            //Checks if the string after the operator is a natural number
+            if (!is_numeric($value) || $value <= 0) {
                 throw new Exception("The price is not a valid number", 400);
             }
 
@@ -55,11 +68,5 @@ class QueryParser
                 'value' => $value
             ]);
         });
-
-        if ($parts->every(fn ($part) => $part->count() == 3)) {
-            return $parts;
-        } else {
-            throw new Exception('One or more of the properties ', 400);
-        }
     }
 }
